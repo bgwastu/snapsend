dab929fd8822318f631534960843bd1e078d51c9
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-08-24 10:33:02 +0700

    Break photo modal into new component

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ import Logo from '../components/Logo';
 import Header from '../components/Logo';
 import MobileUploadButton from '../components/MobileUploadButtons';
 import PhotoPreview from '../components/PhotoPreview';
+import SendPhotoContainer from '../components/SendPhotoContainer';
 import usePhotoStore from '../stores/photo';
 
 const Home: NextPage = ({ uaString }: any) => {
@@ -44,42 +45,7 @@ const Home: NextPage = ({ uaString }: any) => {
         size="xl"
         centered
       >
-        <SimpleGrid
-          cols={2}
-          breakpoints={[
-            { maxWidth: 755, cols: 2, spacing: 'sm' },
-            { maxWidth: 600, cols: 1, spacing: 'sm' },
-          ]}
-        >
-          <PhotoPreview />
-          <Stack>
-            <Input.Wrapper
-              label="Photo Duration"
-              description="Photo expiration time"
-              withAsterisk
-            >
-              <SegmentedControl
-                color="violet"
-                data={[
-                  { label: '3 seconds', value: '3' },
-                  { label: '5 seconds', value: '5' },
-                  { label: '10 seconds', value: '10' },
-                ]}
-              />
-            </Input.Wrapper>
-            <Input.Wrapper
-              label="Caption"
-              description="Write a caption for your photo (optional)"
-            >
-              <Input
-                placeholder="Feeling cute today"
-                size="md"
-                sx={{ flex: 1 }}
-              />
-            </Input.Wrapper>
-            <Button leftIcon={<IconSend />}>Send</Button>
-          </Stack>
-        </SimpleGrid>
+        <SendPhotoContainer />
       </Modal>
       <Container my={20}>
         <Logo />
