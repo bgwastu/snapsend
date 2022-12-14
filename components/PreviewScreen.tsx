300de5c4e439d6666a5ec20cdc1ab62132e087ad
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-12-14 08:19:29 +0700

    refactor: more better error message

--- components/PreviewScreen.tsx
+++ components/PreviewScreen.tsx
@@ -169,7 +169,9 @@ const UploadInputs = () => {
           if (res.status === 200) {
             return res.json();
           } else {
-            throw new Error('HTTP error: ' + res.status.toString());
+            throw new Error(
+              res.body?.toString() ?? "HTTP error: " + res.status.toString()
+            );
           }
         })
         .then((body) => {
